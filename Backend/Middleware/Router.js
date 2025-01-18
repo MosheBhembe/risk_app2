5acdabdfd43214c9568d2ee64aa4c15eb611474f
Moshe <moshe.bhembe96@gmail.com>
2025-01-18 11:56:18 +0200

    updated missing near hit router

--- Backend/Middleware/Router.js
+++ Backend/Middleware/Router.js
@@ -6,7 +6,7 @@ const { registerUser, delete_user } = require('../Controllers/Register.js');
 const getAllEmails = require('../Controllers/getEMails.js');
 const forgotPassword = require('../Controllers/forgot_password.js');
 const upload = require('../Middleware/multer_config.js');
-
+const Hit = require('../Controllers/nearhit.js')
 const fuel_data = require('../Controllers/fuelData.js');
 const resetPassword = require('../Controllers/resetPassword.js');
 const tyre = require('../Controllers/TyreChangeData.js');
@@ -21,6 +21,7 @@ router.post('/register-user', registerUser);
 router.post('/delete-user', delete_user);
 router.post('/forgot-password', forgotPassword);
 router.get('/emails', getAllEmails);
+router.post('/near-hit', Hit);
 router.post('/fuel-data', upload.single('image'), fuel_data);
 router.post('/reset-password', resetPassword);
 router.post('/tyre-change', tyre);
