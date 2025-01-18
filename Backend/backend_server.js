443e084bd8619f41bec6b5b43b387f1ff556fcad
Moshe <moshe.bhembe96@gmail.com>
2025-01-18 09:57:01 +0200

    Updated Cors

--- Backend/backend_server.js
+++ Backend/backend_server.js
@@ -8,7 +8,12 @@ const router = require('./Middleware/Router');
 
 // configurations
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "*", 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true
+}));
 
 app.use(express.json());
 app.use('/api', router);
